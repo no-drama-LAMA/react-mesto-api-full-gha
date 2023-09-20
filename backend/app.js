d4dd2ba12be7c8b2b15c1b8a294a53b1696cf3fb
lambkin <alinao.post@yandex.ru>
2023-09-20 21:45:52 +0300

    fix cors2

--- backend/app.js
+++ backend/app.js
@@ -13,7 +13,13 @@ const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.en
 const app = express();
 
 app.use(cors({
-  origin: 'https://react.mesto.api.full.nomoredomainsrocks.ru/',
+  origin: ''
+  origin: [
+    'https://react.mesto.api.full.nomoredomainsrocks.ru',
+    'http://react.mesto.api.full.nomoredomainsrocks.ru',
+  ],
+  methods: ['GET', 'PUT', 'POST', 'DELETE'],
+  allowedHeaders: ['Authorization', 'Content-Type'],
 }));
 
 app.use(helmet()); // Заголовки безопасности можно проставлять автоматически
